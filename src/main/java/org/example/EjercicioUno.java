40da2de4b9eb1c0ba3634cc8336400f501a1deac
Martin Guillen <guillenmartin94@gmail.com>
2022-08-12 17:52:51 -0300

    Se agrega el enunciado del Ejercicio Uno

--- src/main/java/org/example/EjercicioUno.java
+++ src/main/java/org/example/EjercicioUno.java
@@ -1,6 +1,13 @@
 package org.example;
 
 public class EjercicioUno {
+
+    private String enunciado = "Crear un programa para que permita ingresar el nombre del alumno, y responda con el mensaje: \"Bienvenido [alumno] a TSB\"";
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
     public void saludar(String nombre) {
         System.out.println("Bienvenido "+nombre+" a TSB!");
     }
