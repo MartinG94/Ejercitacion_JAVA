2e13ae12862c7e56bc7473b9982e78b19b401f62
Martin Guillen <guillenmartin94@gmail.com>
2022-08-12 18:05:31 -0300

    Quitando saltos de linea innecesarios

--- src/main/java/org/example/EjercicioDos.java
+++ src/main/java/org/example/EjercicioDos.java
@@ -2,15 +2,14 @@ package org.example;
 
 public class EjercicioDos {
 
-    private String enunciado = "Desarrollar un programa con menú de opciones que implemente lo siguiente:\n" +
-            "\n" +
-            "Generar en un arreglo n elementos numéricos aleatorios (pueden existir duplicados) y  ordenarlo en forma ascendente\n" +
-            "\n" +
-            "Mostrar el arreglo\n" +
-            "\n" +
+    private String enunciado = "Desarrollar un programa con menú de opciones que implemente lo siguiente:\n"+
+            "Generar en un arreglo n elementos numéricos aleatorios (pueden existir duplicados) y  ordenarlo en forma ascendente\n"+
+            "Mostrar el arreglo\n"+
             "Buscar un elemento x dentro del arreglo (x se ingresa por teclado). Si no existe, informarlo. Si existe, cambiar su signo.";
 
     public String getEnunciado() {
         return enunciado;
     }
+
+
 }
