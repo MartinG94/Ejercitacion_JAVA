2524a03d370e10bce0dd6c67fa0862bfe312bc7e
Martin Guillen <guillenmartin94@gmail.com>
2022-08-13 13:09:28 -0300

    Concatenacion reemplazada por Text Block

--- src/main/java/org/example/EjercicioDos.java
+++ src/main/java/org/example/EjercicioDos.java
@@ -7,12 +7,14 @@ import java.util.Arrays;
 
 public class EjercicioDos {
 
-    private String enunciado = "Desarrollar un programa con menú de opciones que implemente lo siguiente:\n"+
-            "Generar en un arreglo n elementos numéricos aleatorios (pueden existir duplicados) y  ordenarlo en forma ascendente\n"+
-            "Mostrar el arreglo\n"+
-            "Buscar un elemento x dentro del arreglo (x se ingresa por teclado). Si no existe, informarlo. Si existe, cambiar su signo.";
+    private String enunciado = """
+            Desarrollar un programa con menú de opciones que implemente lo siguiente:
+            Generar en un arreglo n elementos numéricos aleatorios (pueden existir duplicados) y  ordenarlo en forma ascendente
+            Mostrar el arreglo
+            Buscar un elemento x dentro del arreglo (x se ingresa por teclado). Si no existe, informarlo. Si existe, cambiar su signo.""";
 
     private int[] arrayDeNumeros;
+
     public String getEnunciado() {
         return enunciado;
     }
