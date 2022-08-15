93fe0a2de9c33687165a484f3422918efc272665
Martin Guillen <guillenmartin94@gmail.com>
2022-08-15 17:47:34 -0300

    Codigo de prueba

--- src/main/java/org/example/ejercicio3/EjercicioTres.java
+++ src/main/java/org/example/ejercicio3/EjercicioTres.java
@@ -6,7 +6,10 @@ public class EjercicioTres {
 
     public void mostrarEnunciado() {}
 
-    public void funcionalidad() {}
+    public void funcionalidad() {
+        Persona martin = new Persona("Martin", "Guillen");
+        System.out.println(martin);
+    }
 
     public void generarDatosArbitrarios() {}
 }
