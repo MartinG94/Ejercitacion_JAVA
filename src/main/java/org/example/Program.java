4d07de820f5a61212ec66199900487820958d335
Martin Guillen <guillenmartin94@gmail.com>
2022-08-15 11:49:49 -0300

    Estructura para el caso 3

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.ejercicio2.EjercicioDos;
 import org.example.ejercicio1.EjercicioUno;
+import org.example.ejercicio3.EjercicioTres;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -17,6 +18,7 @@ public class Program {
             System.out.println("--- MENU DE OPCIONES ---");
             System.out.println("1. Bienvenido");
             System.out.println("2. Ordenar y Buscar");
+            System.out.println("3. Sala de Reuniones");
             System.out.println("0. Salir");
             try{
                 System.out.println("Escribe una de las opciones");
@@ -32,6 +34,11 @@ public class Program {
                         EjercicioDos ej2 = new EjercicioDos();
                         ej2.mostrarEnunciado();
                         ej2.funcionalidad();
+                    }
+                    case 3 -> {
+                        EjercicioTres ej3 = new EjercicioTres();
+                        ej3.mostrarEnunciado();
+                        ej3.funcionalidad();
                     }
                     case 0 -> salir = true;
                     default -> System.out.println("Solo puedes elegir el 1 o el 2");
