976e75b39a85bd35125b53a5a1e9197d6d726ba6
Martin Guillen <guillenmartin94@gmail.com>
2022-08-15 16:56:16 -0300

    Propiedades de una Sala

--- src/main/java/org/example/ejercicio3/solucion/Sala.java
+++ src/main/java/org/example/ejercicio3/solucion/Sala.java
@@ -2,4 +2,23 @@ package org.example.ejercicio3.solucion;
 
 public class Sala {
 
+    private int identificador;
+
+    private Reunion[] reuniones;
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public Reunion[] getReuniones() {
+        return reuniones;
+    }
+
+    public void setReuniones(Reunion[] reuniones) {
+        this.reuniones = reuniones;
+    }
 }
