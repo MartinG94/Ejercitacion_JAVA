cfdd98c78d832ab93393241bf7c6ffc22e2fc409
Martin Guillen <guillenmartin94@gmail.com>
2022-08-15 16:55:25 -0300

    Propiedades de una Reunion

--- src/main/java/org/example/ejercicio3/solucion/Reunion.java
+++ src/main/java/org/example/ejercicio3/solucion/Reunion.java
@@ -2,4 +2,23 @@ package org.example.ejercicio3.solucion;
 
 public class Reunion {
 
+    private int horaInicio;
+
+    private Persona[] participantes;
+
+    public int getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(int horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public Persona[] getParticipantes() {
+        return participantes;
+    }
+
+    public void setParticipantes(Persona[] participantes) {
+        this.participantes = participantes;
+    }
 }
