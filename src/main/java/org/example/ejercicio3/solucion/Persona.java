58cff6c0283528be708ff2e446e979a0b76433f7
Martin Guillen <guillenmartin94@gmail.com>
2022-08-15 15:06:13 -0300

    Propiedades nombre y apellido

--- src/main/java/org/example/ejercicio3/solucion/Persona.java
+++ src/main/java/org/example/ejercicio3/solucion/Persona.java
@@ -2,4 +2,22 @@ package org.example.ejercicio3.solucion;
 
 public class Persona {
 
+    private String nombre;
+    private String apellido;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
